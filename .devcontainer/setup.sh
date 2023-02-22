434268f76ec4455050b2c8f9f3e6251256d8e46c
Fredrik Klingenberg <fredrkl@gmail.com>
2023-02-22 19:08:18 +0100

    ADD: Demo showing using scripts
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install bash-completion
+sudo apt-get install -y vim
+echo "command -v flux >/dev/null && . <(flux completion bash)" >> ~/.bashrc
+git config commit.template .gitcommitmsg.txt
+wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.19.5/kubeseal-0.19.5-linux-amd64.tar.gz
+tar -xvzf kubeseal-0.19.5-linux-amd64.tar.gz kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
+rm kubeseal
+rm kubeseal-0.19.5-linux-amd64.tar.gz
