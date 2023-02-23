551758ca0e9f55cee8632533a1fb4ecddc35b626
Fredrik Klingenberg <fredrkl@gmail.com>
2023-02-23 12:45:02 +0100

    ADD: Cleanup
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,10 +1,8 @@
 sudo apt-get update
-sudo apt-get install bash-completion
-sudo apt-get install -y vim
+sudo apt-get install -y bash-completion vim
 echo "command -v flux >/dev/null && . <(flux completion bash)" >> ~/.bashrc
 git config commit.template .gitcommitmsg.txt
 wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.19.5/kubeseal-0.19.5-linux-amd64.tar.gz
 tar -xvzf kubeseal-0.19.5-linux-amd64.tar.gz kubeseal
 sudo install -m 755 kubeseal /usr/local/bin/kubeseal
-rm kubeseal
-rm kubeseal-0.19.5-linux-amd64.tar.gz
+rm kubeseal kubeseal-0.19.5-linux-amd64.tar.gz
